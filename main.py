3d05407e70e6fd04a4b7e7d4069d2b7e43a82f47
dia-meTr <diana.tryhub@gmail.com>
2022-12-03 20:53:19 +0200

    MYsql connection added

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import mysql.connector
 from constants import *
+import tkinter as tk
+from tkinter import ttk
+from working_area import Main
+
 
 mydb = mysql.connector.connect(
-    host=HOST,
-    user=USER,
-    password=PASSWORD,
-    database=DATABASE
+    host=connection_params['host'],
+    user=connection_params['user'],
+    password=connection_params['password'],
+    database=connection_params['database']
 )
+
+print(mydb)
 
 mycursor = mydb.cursor()
 
-mycursor.execute("Show tables;")
+mycursor.execute("SELECT * from drink")
 
 myresult = mycursor.fetchall()
 
